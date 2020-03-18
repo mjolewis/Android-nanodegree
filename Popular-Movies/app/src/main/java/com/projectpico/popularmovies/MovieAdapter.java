2a4324973a9d0463e312ab54a6b77e743846a88e
Michael Lewis <mjolewis@gmail.com>
2020-03-18 11:16:08 -0400

    FEAT: Add movie object and image loading to our adapter
    -Movie object contains meta data for each movie in our data set.
    
    -Use Picasso to load images into our recycler view.

--- Popular-Movies/app/src/main/java/com/projectpico/popularmovies/MovieAdapter.java
+++ Popular-Movies/app/src/main/java/com/projectpico/popularmovies/MovieAdapter.java
@@ -1,15 +1,15 @@
 package com.projectpico.popularmovies;
 
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -19,34 +19,24 @@ import java.util.ArrayList;
  * @author mlewis 
  * @version March 16, 2020
  *********************************************************************************************************************/
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
     // Invariant of the MovieAdapter.java class
-    //  1. The instance variable itemCount is required by the concrete class getItemCount when extending a RecyclerView
-    //    Adapter. It provides a count of the number of items in our data source.
-    //  2. The instance variables movieTitle, movieReleaseDate, movieVoteAverage, moviePlot, and moviePoster are
-    //    references to the data items that we retrieve from our data set.
-    //  3. The class variable TAG is used for debugging purposes.
-    private int itemCount;
-    private String movieTitle;
-    private String movieReleaseDate;
-    private String movieVoteAverage;
-    private String moviePlot;
-    private String moviePoster;
+    //  1. The instance variable moviePoster is a reference to the poster paths for the movie images.
+    //  2. The class variable TAG is used for debugging purposes.
+    private String[] posterPaths;
     private static final String TAG = MovieAdapter.class.getSimpleName();
 
     /**
      * public MovieAdapter(ArrayList<String> json)
      *  Initializes a custom Adapter, which provides a binding from our movies API data set to views that are displayed
      *  within the apps RecyclerView.
-     * @param json
+     * @param posterPaths
+     *  An array of poster paths for each movie image.
      * @exception OutOfMemoryError
      *  Indicates insufficient memory for a new MovieAdapter.
      */
-    public MovieAdapter(ArrayList<String> json) {
-        this.movieReleaseDate = json.get(0);
-        this.moviePoster = json.get(1);
-        this.movieVoteAverage = json.get(2);
-        this.moviePlot = json.get(1);
+    public MovieAdapter(String[] posterPaths) {
+        this.posterPaths = posterPaths;
     }
 
     /**
@@ -57,7 +47,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
      */
     @Override
     public int getItemCount() {
-        return itemCount;
+        return posterPaths.length;
     }
 
     /**
@@ -74,10 +64,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
      */
     @NonNull
     @Override
-    public MovieAdapter.MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MovieAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         CardView cardView = (CardView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_movie_detail, parent, false);
-        return new MovieViewHolder(cardView);
+                .inflate(R.layout.card_movie_image, parent, false);
+        return new ViewHolder(cardView);
     }
 
     /**
@@ -90,7 +80,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
      *  The position of the item within the adapter's data set.
      */
     @Override
-    public void onBindViewHolder(@NonNull MovieAdapter.MovieViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MovieAdapter.ViewHolder holder, int position) {
         Log.d(TAG, "Element " + position + " set.");
 
         /*
@@ -98,15 +88,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
          * our data set in the specified position
          */
         holder.getAdapterPosition();
+        CardView cardView = holder.cardView;
+        ImageView imageView = (ImageView) cardView.findViewById(R.id.iv_movie_poster);
 
-
-        holder.getTitle().setText(movieTitle);
-        holder.getReleaseDate().setText(movieReleaseDate);
-        holder.getVoteAverage().setText(movieVoteAverage);
-        holder.getMoviePlot().setText(moviePlot);
-        holder.getMoviePoster().setText(moviePoster);
-
-        // FIXME: 3/16/20 bind the position
+        Picasso.get()
+                .load(posterPaths)
+                //.placeholder()
+                .into(imageView);
+        // FIXME: 3/18/20 use piccaso?
     }
 
     /******************************************************************************************************************
@@ -116,13 +105,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
      * @author mlewis
      * @version March 16, 2020
      *****************************************************************************************************************/
-    public static class MovieViewHolder extends RecyclerView.ViewHolder {
-        private final CardView cardView;
-        private final TextView title;
-        private final TextView releaseDate;
-        private final TextView voteAverage;
-        private final TextView moviePlot;
-        private final TextView moviePoster;
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView;
 
         /**
          * public MovieViewHolder(CardView view)
@@ -131,62 +115,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
          * @exception OutOfMemoryError
          *  Indicates insufficient memory for a new MovieViewHolder.
          */
-        public MovieViewHolder(CardView view) {
+        public ViewHolder(CardView view) {
             super(view);
-            cardView = view.findViewById(R.id.cv_movies);
-            title = view.findViewById(R.id.tv_movie_title);
-            releaseDate = view.findViewById(R.id.tv_movie_release_date);
-            voteAverage = view.findViewById(R.id.tv_movie_vote_average);
-            moviePlot = view.findViewById(R.id.tv_movie_plot);
-            moviePoster = view.findViewById(R.id.iv_movie_poster);
+            cardView = view;
         }
-
-        /**
-         * public CardView getCardView()
-         *  Accessor method to retrieve the CardView reference variable.
-         * @return CardView
-         *  The id for the CardView layout file.
-         */
-        public CardView getCardView() { return cardView; }
-
-        /**
-         * public TextView getTitle()
-         *  Accessor method to retrieve the movie title reference variable.
-         * @return TextView
-         *  The id for the movie title layout file.
-         */
-        public TextView getTitle() { return title; }
-
-        /**
-         * public TextView getReleaseDate()
-         *  Accessor method to retrieve the release date reference variable.
-         * @return TextView
-         *  The id for the release date layout file.
-         */
-        public TextView getReleaseDate() { return releaseDate; }
-
-        /**
-         * public TextView getVoteAverage()
-         *  Accessor method to retrieve the movie vote average reference variable.
-         * @return TextView
-         *  The id for the movie vote average layout file.
-         */
-        public TextView getVoteAverage() { return voteAverage; }
-
-        /**
-         * public TextView getMoviePlot()
-         *  Accessor method to retrieve the movie plot reference variable.
-         * @return TextView
-         *  The id for the movie plot layout file.
-         */
-        public TextView getMoviePlot() { return moviePlot; }
-
-        /**
-         * public ImageView getMoviePoster()
-         *  Accessor method to retrieve the movie poster reference variable.
-         * @return ImageView
-         *  The id for the movie poster layout file.
-         */
-        public TextView getMoviePoster() { return moviePoster; }
     }
 }
